ffece70850ec75d5ca8670c40a91a14d9125d8da
iMooke <51692229@qq.com>
2018-04-24 09:19:10 +0000

    tinker

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,10 @@
 */
 
 Route::get('/', 'StaticPagesController@home')->name('home');
-Route::get('/help', 'StaticPagesController@help')->name('help');
-Route::get('/about', 'StaticPagesController@about')->name('about');
-Route::get('/signup', 'UsersController@create')->name('signup');
+Route::get('help', 'StaticPagesController@help')->name('help');
+Route::get('about', 'StaticPagesController@about')->name('about');
 
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
 
 
